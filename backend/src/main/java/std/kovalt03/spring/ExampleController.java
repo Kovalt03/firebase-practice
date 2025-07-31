557e8a312fffdbc6034446b6216ce30780315637
김민제 <loloy@Kovalt03.local>
2025-08-01 01:10:19 +0900

    back

--- backend/src/main/java/std/kovalt03/spring/ExampleController.java
+++ backend/src/main/java/std/kovalt03/spring/ExampleController.java
@@ -1,9 +1,11 @@
 package std.kovalt03.spring;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "*")
 public class HelloController {
 
     @GetMapping("/api/hello")
