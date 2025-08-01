fc5340939e0422cfbe420fe1213d5935af673a15
김민제 <loloy@Kovalt03.local>
2025-08-01 13:04:20 +0900

    last

--- backend/src/main/java/std/kovalt03/Controller/LoginController.java
+++ backend/src/main/java/std/kovalt03/Controller/LoginController.java
@@ -1,7 +1,6 @@
 package std.kovalt03.controller;
 
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseHeader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import std.kovalt03.dto.MessageRequest;
@@ -10,7 +9,7 @@ import std.kovalt03.dto.MessageRequest;
 public class LoginController {
 
     @PostMapping("/login")
-    public String Response(@RequestBody MessageRequest request) {
+    public ResponseEntity<String> Response(@RequestBody MessageRequest request) {
         String username = request.getUsername();
         String password = request.getPassword();
 
@@ -18,12 +17,12 @@ public class LoginController {
         headers.add("Set-Cookie", "sessionId=1234; Path=/; HttpOnly; Secure; SameSite=Lax");
 
         String body = "id: " + request.getUsername() + ", password: " + request.getPassword();
-        if (username == "admin" && password == "1234") {
+        if (username.equals("admin") && password.equals("1234")) {
             return ResponseEntity.ok()
                     .headers(headers)
                     .body(body);
         }
-        return ResponseEntity.ok()
+        return ResponseEntity.status(401)
                 .body("There is no user with this username and password");
     }
 }
