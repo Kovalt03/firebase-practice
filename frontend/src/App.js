4e7d1b881764b281994fb5d3ed8ca986b174514c
김민제 <loloy@Kovalt03.local>
2025-08-01 12:22:23 +0900

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import Login from './account/Login';
 function App() {
   const [name, setName] = useState('');
   const [message, setMessage] = useState('');
--- frontend/src/account/Login.js
+++ frontend/src/account/Login.js
@@ -48,3 +48,5 @@ const Login = () => {
         </form>
     );
 }
+
+export default Login;
