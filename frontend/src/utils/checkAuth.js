7fadaeb4d8af46475af8a95da48e160f9ccd3b6b
김민제 <loloy@Kovalt03.local>
2025-08-08 15:45:05 +0900

    front

--- frontend/src/utils/checkAuth.js
+++ frontend/src/utils/checkAuth.js
@@ -2,18 +2,31 @@ const BASE_URL = "https://firebase-practice-kcf3.onrender.com";
 
 export const checkAuth = async () => {
   const res = await fetch(`${BASE_URL}/check-auth`, {
-    method: 'GET',
-    credentials: 'include'
+    method: 'POST',
+    credentials: 'include',
+    body: JSON.stringify({
+      username: data.username
+    }),
   });
+
+  if (!res.ok) {
+    alert('You are not authenticated. Please log in.');
+    return 0;
+  }
+
   const data = await res.json();
+
+  console.log(data);
   if(data.role === 'admin') {
     return 102; // Admin role
   }
   if(data.role === 'user') {
+    alert(`Welcome back, ${data.username}!`);
     return 1; // User role
   }
   if(data.role === 'banned') {
     return 2; // Banned role
   }
+  alert('You are not authenticated. Please log in.');
   return 0; // Not authenticated
 };
